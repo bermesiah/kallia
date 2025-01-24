a83dc2fa3b5d397ace86a8adab6d16b7284dacc4
ckwong <hickwong@gmail.com>
2025-01-24 22:51:43 +0800

    generate assistants resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
 import { ThreadsModule } from './threads/threads.module';
+import { AssistantsModule } from './assistants/assistants.module';
 
 @Module({
-  imports: [MessagesModule, ThreadsModule],
+  imports: [MessagesModule, ThreadsModule, AssistantsModule],
   controllers: [AppController],
   providers: [AppService],
 })
