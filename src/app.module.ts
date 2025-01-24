f0cca9e4698219d9369f01199b7dbdb73ff12aba
ckwong <hickwong@gmail.com>
2025-01-24 22:34:20 +0800

    generate threads resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
+import { ThreadsModule } from './threads/threads.module';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [MessagesModule, ThreadsModule],
   controllers: [AppController],
   providers: [AppService],
 })
