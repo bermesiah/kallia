00ca20baa60ddbc2de338e76a34bb30819431a7a
ckwong <hickwong@gmail.com>
2025-01-26 12:45:41 +0800

    generate steps resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { MessagesModule } from './messages/messages.module';
 import { ThreadsModule } from './threads/threads.module';
 import { AssistantsModule } from './assistants/assistants.module';
 import { RunsModule } from './runs/runs.module';
+import { StepsModule } from './steps/steps.module';
 
 @Module({
-  imports: [MessagesModule, ThreadsModule, AssistantsModule, RunsModule],
+  imports: [MessagesModule, ThreadsModule, AssistantsModule, RunsModule, StepsModule],
   controllers: [AppController],
   providers: [AppService],
 })
