86a688816a3b25bfe86c35456175a19f0b15a3a1
ckwong <hickwong@gmail.com>
2025-01-26 12:43:50 +0800

    generate runs resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
 import { ThreadsModule } from './threads/threads.module';
 import { AssistantsModule } from './assistants/assistants.module';
+import { RunsModule } from './runs/runs.module';
 
 @Module({
-  imports: [MessagesModule, ThreadsModule, AssistantsModule],
+  imports: [MessagesModule, ThreadsModule, AssistantsModule, RunsModule],
   controllers: [AppController],
   providers: [AppService],
 })
